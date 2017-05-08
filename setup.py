9c1ebb85cdd839ea596fa982b1554c3780de7680
jciskey <jciskey@gmail.com>
2017-05-07 21:54:52 -0500

    Add author and author_email to configuration

--- setup.py
+++ setup.py
@@ -4,10 +4,12 @@ EXCLUDE_FROM_PACKAGES = []
 
 setup(
     name='PyGraph',
-    version='0.2.0',
+    version='0.2.1',
     description="A graph manipulation library in pure Python",
     url="https://github.com/jciskey/pygraph",
     license="MIT",
+    author="Joe Ciskey",
+    author_email="forms@sleeplesshacker.com",
     packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
